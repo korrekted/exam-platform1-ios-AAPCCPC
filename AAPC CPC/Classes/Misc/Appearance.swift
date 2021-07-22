fa9d2b414b0d77ec6f319287f086f61ba88a5459
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-22 21:26:34 +0300

    init

---
+++ AAPC CPC/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 250, green: 251, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 2, green: 83, blue: 162)
+    static let mainColorAlpha = UIColor(integralRed: 2, green: 83, blue: 162, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 147, green: 206, blue: 245)
+    
+    static let progress1Color = UIColor(integralRed: 2, green: 83, blue: 162)
+    static let progress2Color = UIColor(integralRed: 147, green: 206, blue: 245)
+    static let progress3Color = UIColor(integralRed: 22, green: 63, blue: 89)
+}
