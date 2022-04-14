74b08de549dde3694b82c9c75504974a8befef30
akonst17 <akonst17@gmail.com>
2022-04-14 14:10:10 +0300

    upgrade; build 8

--- AAPC CPC/Classes/Misc/Appearance.swift
+++ AAPC CPC/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 2, green: 83, blue: 162)
     static let progress2Color = UIColor(integralRed: 147, green: 206, blue: 245)
     static let progress3Color = UIColor(integralRed: 22, green: 63, blue: 89)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
